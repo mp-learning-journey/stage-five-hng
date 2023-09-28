b0e7ff29f25af55bda3acba2954be501b9ad4508
mrprotocoll <lekanvgbg@gmail.com>
2023-09-28 16:54:12 +0100

    Create recording model and controller

---
+++ app/Http/Controllers/Api/V1/RecordingController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\V1\StoreRecordingRequest;
+use App\Http\Requests\V1\UpdateRecordingRequest;
+use App\Models\Recording;
+
+class RecordingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRecordingRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Recording $recording)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Recording $recording)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRecordingRequest $request, Recording $recording)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Recording $recording)
+    {
+        //
+    }
+}
