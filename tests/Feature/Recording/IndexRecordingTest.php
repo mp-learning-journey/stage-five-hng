1eb9401fb64aeb091fa2afb84b2b3b05e89f77c6
mrprotocoll <lekanvgbg@gmail.com>
2023-09-29 08:40:20 +0100

    Add recording show endpoint test

--- tests/Feature/Recording/IndexRecordingTest.php
+++ tests/Feature/Recording/IndexRecordingTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Recording;
 
+use App\Models\Recording;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -16,7 +17,7 @@ class IndexRecordingTest extends TestCase
         $recording1 = Recording::factory()->create();
         $recording2 = Recording::factory()->create();
 
-        $response = $this->getJson('/recordings');
+        $response = $this->getJson($this->url);
 
         $response->assertStatus(200)
             ->assertJsonStructure([
@@ -48,7 +49,7 @@ class IndexRecordingTest extends TestCase
 
     public function test_can_list_recordings_empty()
     {
-        $response = $this->getJson('/recordings');
+        $response = $this->getJson($this->url);
 
         $response->assertStatus(200)
             ->assertJsonStructure([
@@ -58,19 +59,4 @@ class IndexRecordingTest extends TestCase
             ]);
     }
 
-    public function test_cannot_list_recordings_on_error()
-    {
-        // Mock an error or exception scenario
-        // For example, create a scenario where the database connection is unavailable
-
-        $this->mockDatabaseError(); // Implement your custom mock
-
-        $response = $this->getJson('/recordings');
-
-        $response->assertStatus(500)
-            ->assertJsonStructure([
-                'error',
-                'statusCode',
-            ]);
-    }
 }
--- tests/Feature/Recording/ShowRecordingTest.php
+++ tests/Feature/Recording/ShowRecordingTest.php
@@ -2,19 +2,49 @@
 
 namespace Tests\Feature\Recording;
 
+use App\Models\Recording;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ShowRecordingTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
+    use RefreshDatabase;
+    public function test_can_show_recording()
     {
-        $response = $this->get('/');
+    // Create a test recording in the database
+    $recording = Recording::factory()->create();
 
-        $response->assertStatus(200);
+    $response = $this->getJson($this->url."/{$recording->id}");
+
+    $response->assertStatus(200)
+        ->assertJsonStructure([
+            'data' => [
+                'id',
+                'title',
+                'url',
+                'description',
+                'fileName',
+                'fileSize',
+                'thumbnail',
+                'slug',
+                'createdAt',
+            ],
+        ])
+        ->assertJsonFragment([
+            'id' => $recording->id,
+            'title' => $recording->title,
+        ]);
+    }
+
+    public function test_cannot_show_nonexistent_recording()
+    {
+        $response = $this->getJson($this->url."/nonExistentId}");
+
+        $response->assertStatus(404)
+            ->assertJsonStructure([
+                'error',
+                'statusCode',
+            ]);
     }
 }
