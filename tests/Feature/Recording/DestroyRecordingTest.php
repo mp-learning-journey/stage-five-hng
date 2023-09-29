68ede701c98335931351bf11e62b44aafb05a938
mrprotocoll <lekanvgbg@gmail.com>
2023-09-29 08:54:14 +0100

    Add recording destroy endpoint test

--- tests/Feature/Recording/DestroyRecordingTest.php
+++ tests/Feature/Recording/DestroyRecordingTest.php
@@ -2,19 +2,61 @@
 
 namespace Tests\Feature\Recording;
 
+use App\Models\Recording;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\DB;
 use Tests\TestCase;
 
 class DestroyRecordingTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
+    use RefreshDatabase;
+    public function test_can_delete_recording()
     {
-        $response = $this->get('/');
+        // Create a test recording in the database
+        $recording = Recording::factory()->create();
 
-        $response->assertStatus(200);
+        $response = $this->deleteJson($this->url."/{$recording->id}");
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'message',
+                'statusCode',
+            ]);
+
+        $this->assertSoftDeleted('recordings', ['id' => $recording->id]);
     }
+
+    public function test_cannot_delete_nonexistent_recording()
+    {
+        $response = $this->deleteJson($this->url."/nonexistent_id");
+
+        $response->assertStatus(404)
+            ->assertJsonStructure([
+                'error',
+                'statusCode',
+            ]);
+    }
+
+    public function test_cannot_delete_recording_on_error()
+    {
+        // Create a test recording in the database
+        $recording = Recording::factory()->create();
+
+        // Mock the database delete operation to throw an exception
+        DB::shouldReceive('delete')
+            ->once()
+            ->with('DELETE FROM recordings WHERE id = ?', [$recording->id])
+            ->andThrow(new \Exception('Database error'));
+
+        $response = $this->deleteJson($this->url."/{$recording->id}");
+
+        $response->assertStatus(500)
+            ->assertJsonStructure([
+                'error',
+                'statusCode',
+            ]);
+    }
+
+
 }
